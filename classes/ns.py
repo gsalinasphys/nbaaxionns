Fixed units

classes/ns.py
@@ -4,7 +4,7 @@ import numpy as np
 from numba import float64  # import the types
 from numba.experimental import jitclass
 from scripts.basic import cases, heav, mag, mydot, nums_vs
-from scripts.globals import G, maGHz
+from scripts.globals import G, G_eV2, e, hbar, ma, me
 
 spec = [
     ('mass', float64),
@@ -60,10 +60,10 @@ class NeutronStar:
         d = mag(position)
         return 3*mydot(position, self.m(time))/d**5*position - self.m(time)/d**3
 
-    def wp(self, position: np.ndarray, time: float) -> float:  # Plasma frequency in GHz
-        return 1.5e2*sqrt(abs(mydot(self.B(position, time), self.axis))/self.T)
+    def wp(self, position: np.ndarray, time: float) -> float:  # Plasma frequency in eV
+        return sqrt(4*pi*e/me * 1e14*G_eV2*hbar) * sqrt(abs(mydot(self.B(position, time), self.axis))/self.T)
     
     # Maximum radius of conversion with some padding (in percent)
     def rcmax(self, padding: float = 0.) -> float:
-        return (1+padding/100) * 28.231 * self.radius * (self.B0/(self.T*maGHz**2))**(1/3)
+        return (1+padding/100) * 9.88422**(2./3) * self.radius * (self.B0/(self.T*ma**2))**(1/3)
 

scripts/conversion.py
@@ -6,7 +6,7 @@ import numpy as np
 from scipy.optimize import brentq, root_scalar
 
 from scripts.basic import mag, mydot, randdirs3d, zeroat
-from scripts.globals import G_eV2, c, eV_GHz, gag, km_eVinv, maGHz, outdir
+from scripts.globals import ma, outdir
 from scripts.orbits import smoothtraj
 
 
@@ -15,7 +15,7 @@ def rc(NS: object, position: np.ndarray, time: float, exact: bool = False) -> fl
     dir = position/mag(position)
     if not exact:
         def to_min(x: float) -> float:
-            return NS.wp(x*dir, time) - maGHz
+            return 1e5*NS.wp(x*dir, time) - ma
         
     try:
         rc = root_scalar(to_min, bracket=[NS.radius, 100*NS.radius], xtol=1e-10, rtol=1e-10).root